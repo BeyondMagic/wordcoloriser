1b905561e669bf697baf363372607e3be27d9175
J.Farias (BeyondMagic/koetemagie) <koetemagie@gmail.com>
2022-04-06 18:00:20 -0300

    source: remove newline at the end.
    std::endl can be replaced by ignoring last newline of the text variable.

--- source/main.cpp
+++ source/main.cpp
@@ -56,8 +56,8 @@ int main( const int argc, char** argv )
   std::string end = "m$&\033[0m";
 
   std::vector<std::string> colours =
-    { "30", "31", "32", "33", "34", "35", "36", "37",
-      "90", "91", "92", "93", "94", "95", "96", "97"};
+    { "30", "90", "31", "91", "32", "92", "33", "93",
+      "34", "94", "35", "95", "36", "96", "37", "97"};
 
   while ( getline(std::cin, line) ) {
 
@@ -84,7 +84,7 @@ int main( const int argc, char** argv )
 
   }
 
-  std::cout << all << std::endl;
+  std::cout << all;
 
   exit(0);
 
